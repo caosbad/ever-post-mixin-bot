0171f0b8a8476da9def98961ebaa6ac2bbd463b5
Caos <caosbad@gmail.com>
2018-12-30 11:28:37 +0800

    update file

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+const (
+	BuildVersion   = "0.1.0"
+	HTTPListenPort = 7008
+)
+
+const ( // XIN
+	XINAssetId = "c94ac88f-4671-3976-b60a-09064f1811e8"
+	XINAmount  = "0.01"
+)
+const ( // CNB
+	CNBAssetId = "965e5c6e-434c-3fa9-b780-c50f43cd955c"
+	CNBAmount  = "100"
+)
+
+const (
+	DatabaseUser     = "postgres"
+	DatabasePassword = "postgres"
+	DatabaseHost     = "localhost"
+	DatabaseName     = "postgres"
+	DatabasePort     = 5432
+)
+
+const (
+	ClientId     = ""
+	ClientSecret = ""
+	SessionId    = ""
+	Pin          = ""
+	PinToken     = ""
+	PrivateKey = ""
+)
